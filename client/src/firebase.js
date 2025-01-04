5ba3b9795da84323317c4db961b5eec99338a880
Adityagargiiith <127509487+Adityagargiiith@users.noreply.github.com>
2025-01-05 03:41:59 +0530

    changes

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,11 +5,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "publishhub-72bc9.firebaseapp.com",
-  projectId: "publishhub-72bc9",
-  storageBucket: "publishhub-72bc9.appspot.com",
-  messagingSenderId: "300197009790",
-  appId: "1:300197009790:web:5c9a3b93dfea5f2e797b2f",
+  authDomain: "turboreels-47a72.firebaseapp.com",
+  projectId: "turboreels-47a72",
+  storageBucket: "turboreels-47a72.firebasestorage.app",
+  messagingSenderId: "1053754066277",
+  appId: "1:1053754066277:web:9a0cdf4984b2300125cd8c"
 };
 
 // Initialize Firebase
