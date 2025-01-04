73c98fb269ba7fb69c62df85e4df1949ffbc996e
Adityagargiiith <127509487+Adityagargiiith@users.noreply.github.com>
2025-01-05 03:11:01 +0530

    change

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from 'flowbite-react/tailwind';
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content()
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin'),
-  require('tailwind-scrollbar'),
+  plugins: [
+    import('flowbite/plugin'),
+    import('tailwind-scrollbar'),
   ],
-};
+}
